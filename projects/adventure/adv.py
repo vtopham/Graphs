ca0a531f9f38e289df8a840c58b93b25dec07352
Victoria  Topham <topham.victoria@gmail.com>
2020-07-16 13:01:21 -0700

    got a basic traversal working

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -12,11 +12,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,6 +29,64 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+
+def inv_direction(dir_travelled):
+    if dir_travelled == 'n':
+        return 's'
+    if dir_travelled == 's':
+        return 'n'
+    if dir_travelled == 'e':
+        return 'w'
+    if dir_travelled == 'w':
+        return 'e'
+
+graph = {}
+
+def get_path_baby(player, room_from, dir_travelled, graph):
+    #just get a depth first traversal working
+    #Get a dictionary ready to go of all rooms, starting with
+    print(f"moving {dir_travelled}")
+    
+    
+    cur_room = player.current_room.id
+    #unless we're starting, move the player
+    if dir_travelled != None:
+        player.travel(dir_travelled)
+        cur_room = player.current_room.id
+    print(f"we are in room {cur_room}")
+    #if this room isn't in the graph then let's populate it, totally blank
+    if cur_room not in graph:
+        
+        rooms = player.current_room.get_exits()
+       
+        graph[cur_room] = {}
+        for room in rooms:
+            graph[cur_room][room] = "?"
+       
+    #add the edges for this room and the room that we came from
+    if room_from != None:
+        graph[room_from][dir_travelled] = cur_room
+        graph[cur_room][inv_direction(dir_travelled)] = room_from
+
+    for direction in graph[cur_room]:
+        if graph[cur_room][direction] == "?":
+            get_path_baby(player, cur_room, direction, graph)
+
+            
+    print(graph)
+    return None
+
+    return graph
+    #for each direction, if you can go in that direction, recurse
+
+
+   
+    
+
+
+result = get_path_baby(player, None, None, graph)
+print(result)
+
 traversal_path = []
 
 
@@ -53,12 +111,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
